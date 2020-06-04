12ee37d805c6b9e24d9f93306d204e70c1fa5ca7
Ryan Gaudet <rgaudet@vtexvsi.com>
2020-06-04 10:45:02 -0500

    refactor: Had to re-add deleteTempAudio file code to completePlayer();

--- data/main/transcribe.js
+++ data/main/transcribe.js
@@ -208,10 +208,7 @@ function clear() {
     $('#report').garlic('destroy');
     document.title = 'Form';
     tinyMCE.activeEditor.setContent('');
-    //Delete Temp Audio File
-    var fullAudioSrc = AblePlayerInstances[0].media.src;
-    var tempAudioFileName = fullAudioSrc.split("/").pop();
-    clearTempAudio(tempAudioFileName);
+
 
     completePlayer();
 
@@ -267,6 +264,10 @@ $(document).ready(function () {
 function completePlayer() {
     var $loadBtn = $('#loadBtn');
     var $completeBtn = $('#completeBtn');
+    //Delete Temp Audio File
+    var fullAudioSrc = AblePlayerInstances[0].media.src;
+    var tempAudioFileName = fullAudioSrc.split("/").pop();
+    clearTempAudio(tempAudioFileName);
 
     try {
         vScriptCallback("AudioComplete", g_fileName); //For vScription Application to move file to complete folder
