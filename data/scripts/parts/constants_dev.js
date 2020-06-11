7ceccb17c41dd3e25fb1601ca7e44acdc27a4e41
Hossam <hacker2894@gmail.com>
2020-06-11 05:11:48 +0200

    chore: test dev branch

--- data/scripts/parts/constants_dev.js
+++ data/scripts/parts/constants_dev.js
@@ -1,5 +1,5 @@
 // Constants file
-
+//test
 // used in transcribe.js imported inside transcribe.php
 // const audioTempFolder = "https://pro.vtex/vscription/workingTemp/"
 const audioTempFolder = "https://vscriptiontranscribeupload.local:8888/workingTemp/"
