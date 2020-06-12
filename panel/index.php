0a90be094f69ca0e368e212daf330dc849b5990b
Hossam <hacker2894@gmail.com>
2020-06-12 20:10:11 +0200

    refactor: adding an index to admin panel for later additions

--- panel/index.php
+++ panel/index.php
@@ -1,28 +1,27 @@
 <?php
 include('../data/parts/head.php');
 
+// admin panel main
 
 //redirect to main
 if ($_SESSION['role'] != "1") {
-//User is a System or Client Administrator
+//User is a System Administrator ONLY
     ob_start();
     header('Location: '."accessdenied.php");
     ob_end_flush();
     die();
 }
-
-
 ?>
 
 <html>
 
 <head>
-    <title>vScription Admin Tools</title>
+    <title>vScription Admin Panel</title>
     <link rel="shortcut icon" type="image/png" href="../data/images/favicon.png"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../data/scripts/admin_tools.js"></script>
     <link href="../data/libs/node_modules/material-components-web/dist/material-components-web.css" rel="stylesheet">
-    <link href="../data/css/admin_tools.css" rel="stylesheet">
+    <link href="../data/css/admin_panel.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="../data/libs/node_modules/material-components-web/dist/material-components-web.js"></script>
     <script src="../data/libs/node_modules/@material/textfield/dist/mdc.textfield.js"></script>
@@ -39,20 +38,15 @@ if ($_SESSION['role'] != "1") {
 
         <table id="header-tbl">
             <tr>
-
-
-
                 <td id="navbtn" align="left" colspan="1">
-                    <!--                        Logged in as: --><?php //echo $_SESSION['uEmail']?><!-- |-->
-                    <!--                    </div>-->
 
-                    <a href="index.php"><i class="fas fa-arrow-left"></i> Back to Admin Panel</a>
+<!--                    <a class="logout" href="main.php"><i class="fas fa-arrow-left"></i> Go back to job list</a>-->
                 </td>
 
                 <td id="logbar" align="right" colspan="1">
                     Logged in as: <?php echo $_SESSION['uEmail'] ?> |
                     <!--                    </div>-->
-                    <a class="logout" href="logout.php">
+                    <a class="logout" href="../logout.php">
                         <i class="fas fa-sign-out-alt"></i>
                         Logout
                     </a>
@@ -62,7 +56,7 @@ if ($_SESSION['role'] != "1") {
             <tr class="spacer"></tr>
             <tr style="margin-top: 50px">
                 <td class="title" align="left" width="450px">
-                    <legend class="page-title">Admin Tools</legend>
+                    <legend class="page-title">Admin Panel</legend>
                 </td>
                 <!--<td align="right" rowspan="2" id="fix-td">
 
@@ -78,7 +72,7 @@ if ($_SESSION['role'] != "1") {
 
         <div class="grid-wrapper">
 
-            <label class="mdc-text-field mdc-text-field--outlined" id="pwd">
+            <!--<label class="mdc-text-field mdc-text-field--outlined" id="pwd">
                 <input type="text" class="mdc-text-field__input" aria-labelledby="my-label-id">
                 <span class="mdc-notched-outline">
                 <span class="mdc-notched-outline__leading"></span>
@@ -87,14 +81,16 @@ if ($_SESSION['role'] != "1") {
                 </span>
                 <span class="mdc-notched-outline__trailing"></span>
                 </span>
-            </label>
+            </label>-->
 
-            <button class="mdc-button mdc-button--raised" id="hashPwd">
+            <h2>Quick Tools</h2>
+
+            <button class="mdc-button mdc-button--raised tools-button" onclick="location.href='admin_tools.php'">
                 <div class="mdc-button__ripple"></div>
                 <i class="material-icons mdc-button__icon" aria-hidden="true"
                 >vpn_key</i
                 >
-                <span class="mdc-button__label">Hash Password</span>
+                <span class="mdc-button__label">Admin Tools</span>
             </button>
 
         </div>
