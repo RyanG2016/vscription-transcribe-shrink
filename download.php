1a248ad131ab108365fc0f263d5d86420752022a
Ryan Gaudet <rgaudet@vtexvsi.com>
2020-06-14 02:11:35 -0500

    feat: Added files table update on file download

--- download.php
+++ download.php
@@ -12,6 +12,69 @@
     }
     $job_id = $_GET['job_id'];
 
+    /*-----Get existing data for job --------*/
+
+    $sql3 = "SELECT times_text_downloaded_date, text_downloaded_date FROM files WHERE job_id = ?
+    AND acc_id = (SELECT account FROM users WHERE email = '" .$_SESSION['uEmail'] ."')";
+    if($stmt3 = mysqli_prepare($con, $sql3))
+    {
+        mysqli_stmt_bind_param($stmt3, "s", $job_id);
+        echo "We bound ok";
+
+        if(mysqli_stmt_execute($stmt3) ){
+            $result = mysqli_stmt_get_result($stmt3);
+            // Check number of rows in the result set
+            if(mysqli_num_rows($result) > 0){
+                echo "We found at least one row for job " . $job_id;
+                // Fetch result rows as an associative array						
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    $times_downloaded = $row['times_text_downloaded_date'];
+                    $times_downloaded++;
+                    $text_downloaded_date = $row['text_downloaded_date'];
+                    echo $times_downloaded;
+                    echo $text_downloaded_date;
+                }
+            } else {
+                echo "<p>No matches found for job " .$job_id . "</p>";
+            }
+        } else {
+            echo "Error executing " .$sql3;
+        }
+    }  else 
+    {
+            echo "ERROR: Could not prepare to execute $sql1. " . mysqli_error($con);
+            //die( "Error in excute: (" .$con->errno . ") " . $con->error);
+    }
+    /*------Update download statistics ------*/
+    if (is_null($text_downloaded_date)) {
+        $text_downloaded_date = date("Y-m-d H:i:s");
+    } 
+    $sql1 = "UPDATE FILES SET times_text_downloaded_date=?, text_downloaded_date=?
+       WHERE job_id = ? AND acc_id = (SELECT account FROM users WHERE email = '" .$_SESSION['uEmail'] ."')";
+
+    if($stmt1 = mysqli_prepare($con, $sql1))
+    {
+        if( !$stmt1->bind_param("sss", $times_downloaded, $text_downloaded_date, $job_id)   )
+        {
+                // die( "Error in bind_param: (" .$con->errno . ") " . $con->error);
+        }
+        $B = mysqli_stmt_execute($stmt1);
+        if($B){
+        $result = mysqli_stmt_get_result($stmt1);
+            echo "Data Updated Successfully!";
+        }
+        else {
+            echo "ERROR: Could not able to execute $sql1. " . mysqli_error($con);
+            //die( "Error in excute: (" .$con->errno . ") " . $con->error);
+        }
+    }
+    else
+    {
+        echo "ERROR: Could not able to execute $sql1. " . mysqli_error($con);
+    }
+
+
+    /*------Generate File download ------*/
     $sql = "SELECT job_document_rtf FROM files WHERE job_id=?";
 
     if($stmt = mysqli_prepare($con, $sql))
@@ -40,4 +103,6 @@
 }  else {
     		//echo "<p>No matches found for job " .$job_id . "</p>";
     }
+
+    header('location: main.php'); //Refresh current page 
 ?>
