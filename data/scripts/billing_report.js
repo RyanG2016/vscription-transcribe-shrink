e9b4c376d536fd13186bc9c6e79504afd3c0d567
Hossam <hacker2894@gmail.com>
2020-06-24 01:16:00 +0200

    fix <billing>: date change handling + format

--- data/scripts/billing_report.js
+++ data/scripts/billing_report.js
@@ -1,21 +1,61 @@
 
 $(document).ready(function () {
 
+    let today = new Date().toISOString().split('T')[0];
+
     // let arg = {
     //     pwd: tf.value
     // };
+    let startDate = $( "#startDate" );
+    let endDate = $( "#endDate" );
+    startDate.datepicker({dateFormat: "yy-mm-dd"});
+    endDate.datepicker({dateFormat: "yy-mm-dd"});
+    
+    startDate.val(today);
+    endDate.val(today);
 
-    $( "#startDate" ).datepicker();
-    $( "#endDate" ).datepicker();
 
-    $.post("/data/parts/backend_request.php", {
-        reqcode: 200
-        // ,args: JSON.stringify(arg)
-    }).done(function (res) {
-        let response = JSON.parse(res);
-        let data = response.data;
-        // let error = res.error;
-        $('.billing-report-container').append(data);
+    function checkDates(val, startDateGiven) {
+        if(startDateGiven){
+
+            let otherDate = endDate.val();
+            if(val > otherDate){
+                endDate.val(val);
+            }
+
+        }else{ // val = current end date
+            let otherDate = startDate.val();
+            if(val < otherDate)
+            {
+                startDate.val(val);
+            }
+        }
+    }
+
+    startDate.on("change paste keyup", function() {
+        checkDates($(this).val(), true);
     });
+
+
+    endDate.on("change paste keyup", function() {
+        checkDates($(this).val(), false);
+    })
+
+
+    function getData() {
+        $.post("/data/parts/backend_request.php", {
+            reqcode: 200
+            // ,args: JSON.stringify(arg)
+        }).done(function (res) {
+            let response = JSON.parse(res);
+            let data = response.data;
+            // let error = res.error;
+            $('.billing-report-container').append(data);
+        });
+    }
+
+    getData();
+
+
 
 });
