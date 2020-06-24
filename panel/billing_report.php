95785440230929d087bc24153eb8744cbc0fe569
Hossam <hacker2894@gmail.com>
2020-06-24 19:22:24 +0200

    fix <billing>: missing back and logout icons

--- panel/billing_report.php
+++ panel/billing_report.php
@@ -33,7 +33,7 @@ if ($_SESSION['role'] != "1") {
     <title>vScription Billing Reports</title>
     <link rel="shortcut icon" type="image/png" href="../data/images/favicon.png"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+    <link href='../data/fontawesome/css/all.css' type='text/css' rel='stylesheet'/>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
