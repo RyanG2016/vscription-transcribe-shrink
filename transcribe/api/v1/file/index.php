10c1b688276543a840a1a4992a4d913e3983fb9f
Hossam <hacker2894@gmail.com>
2020-07-03 16:46:56 +0200

    feat <File API>: added support for basic authentication

--- transcribe/api/v1/file/index.php
+++ transcribe/api/v1/file/index.php
@@ -5,11 +5,33 @@ require '../../../../api/bootstrap.php';
 include('../../../data/parts/session_settings.php');
 
 require('../../../data/parts/ping.php');
-
+use Src\Controller\LoginController;
 if(!isset($_SESSION['loggedIn']))
 {
-    header("HTTP/1.1 401 ACCESS DENIED");
-    exit();
+    // check for basic auth
+    if(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER["PHP_AUTH_PW"]))
+    {
+        // Auth Headers Available -> try to login
+        $loginController = new LoginController($dbConnection, $_SERVER["REQUEST_METHOD"]);
+        $result = $loginController->processSilentRequest();
+
+        // if login OK the script will resume..
+        $jsonResponse = json_decode($result["body"], true);
+        if ($jsonResponse["error"] != false) { // login failed
+            header($result['status_code_header']);
+            echo $result['body'];
+            exit();
+        }
+    }
+    else{ // no basic auth sent
+        header("HTTP/1.1 401 ACCESS DENIED");
+        $body = json_encode([
+            'error' => true,
+            'msg' => "You don't have permission to access this web page, make sure you are logged in."
+        ]);
+        echo $body;
+        exit();
+    }
 }
 if(isset($_SESSION['counter']))
 {
