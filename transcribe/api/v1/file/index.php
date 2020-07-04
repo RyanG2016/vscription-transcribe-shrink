6696e5028f8ebe608bf34ed77b9e5575a8464c02
Hossam <hacker2894@gmail.com>
2020-07-04 14:25:30 +0200

    refactor <API auth>: moving check auth code to parts/

--- transcribe/api/v1/file/index.php
+++ transcribe/api/v1/file/index.php
@@ -5,34 +5,7 @@ require '../../../../api/bootstrap.php';
 include('../../../data/parts/session_settings.php');
 
 require('../../../data/parts/ping.php');
-use Src\Controller\LoginController;
-if(!isset($_SESSION['loggedIn']))
-{
-    // check for basic auth
-    if(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER["PHP_AUTH_PW"]))
-    {
-        // Auth Headers Available -> try to login
-        $loginController = new LoginController($dbConnection, $_SERVER["REQUEST_METHOD"]);
-        $result = $loginController->processSilentRequest();
-
-        // if login OK the script will resume..
-        $jsonResponse = json_decode($result["body"], true);
-        if ($jsonResponse["error"] != false) { // login failed
-            header($result['status_code_header']);
-            echo $result['body'];
-            exit();
-        }
-    }
-    else{ // no basic auth sent
-        header("HTTP/1.1 401 ACCESS DENIED");
-        $body = json_encode([
-            'error' => true,
-            'msg' => "You don't have permission to access this web page, make sure you are logged in."
-        ]);
-        echo $body;
-        exit();
-    }
-}
+require "../parts/checkAuth.php"; // <-- checking for basic auth before request & if the user is already logged in
 if(isset($_SESSION['counter']))
 {
     unset($_SESSION['counter']);
