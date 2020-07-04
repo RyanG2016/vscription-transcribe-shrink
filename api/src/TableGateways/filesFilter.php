7f2a782c12d8153baf01c14e3bd6fb544117d99b
Hossam <hacker2894@gmail.com>
2020-07-04 21:56:41 +0200

    feat <File API>: multiple values for param support by adding [mul]

--- api/src/TableGateways/filesFilter.php
+++ api/src/TableGateways/filesFilter.php
@@ -39,9 +39,29 @@ function parseParams($addWhereClause = false){
                 case "isBillable":
                 case "billed":
                     $addedEnum ++;
-                    if(!$firstMatch) {$filter .= " AND ";}
-                    $filter .= "$key = '$value'";
-                    $firstMatch = false;
+
+                    // check if filter is multiple values
+                    if(gettype($value) == "array")
+                    {
+                        if(isset($value["mul"]))
+                        {
+                            $values = preg_split('/,/', $value["mul"], -1, PREG_SPLIT_NO_EMPTY);
+                            foreach ($values as $opt)
+                            {
+                                if(!$firstMatch) {$filter .= " OR ";}
+                                $filter .= "$key = '$opt'";
+                                $firstMatch = false;
+                            }
+
+                        }else{
+                            unprocessableFilterResponse();
+                        }
+
+                    }else if(gettype($value) == "string"){
+                        if(!$firstMatch) {$filter .= " AND ";}
+                        $filter .= "$key = '$value'";
+                        $firstMatch = false;
+                    }
                     break;
             }
         }
@@ -51,4 +71,14 @@ function parseParams($addWhereClause = false){
     else{
         return "";
     }
+}
+
+function unprocessableFilterResponse()
+{
+    header('HTTP/1.1 422 Unprocessable Filter');
+    echo json_encode([
+        'error' => true,
+        'msg' => 'Invalid Filter Input'
+    ]);
+    exit();
 }
