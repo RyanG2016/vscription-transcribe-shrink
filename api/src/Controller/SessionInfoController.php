ef40b4b2f904c4b9662abcacd519b5eb3e7189ca
Hossam <hacker2894@gmail.com>
2020-07-06 19:45:16 +0200

    feat <Session-Info>: added session-info endpoint

---
+++ api/src/Controller/SessionInfoController.php
@@ -0,0 +1,58 @@
+<?php
+namespace Src\Controller;
+
+// no Gateway needed as no DB queries needed.
+
+class SessionInfoController {
+
+    private $db;
+    private $requestMethod;
+    public function __construct($db, $requestMethod)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+            case 'POST':
+                $response = $this->getSessionInfo();
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getSessionInfo()
+    {
+        $response['status_code_header'] = "HTTP/1.1 200 OK";
+        $response['body'] =
+        json_encode(
+            array(
+                "first_name" => $_SESSION["fname"],
+                "last_name" => $_SESSION["lname"],
+                "email" => $_SESSION["uEmail"],
+//                "role" => $_SESSION["role"],
+                "account_id" => $_SESSION["role"]
+            )
+        );
+
+//            print_r($_SESSION);
+        return $response;
+    }
+
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
