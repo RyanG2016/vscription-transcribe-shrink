400e222818208b04906c718e5d1c20d600f59365
Hossam <hacker2894@gmail.com>
2020-08-01 18:31:19 +0200

    feat <job lister>: filtering data by drop down

--- transcribe/data/scripts/transcribe.js
+++ transcribe/data/scripts/transcribe.js
@@ -304,7 +304,28 @@ $(document).ready(function () {
                     return new Date(data * 1000).toISOString().substr(11, 8);
                 }
             }
-        ]
+        ],
+
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
     } );
 
     jobsDT.on( 'draw.dt', function () {
