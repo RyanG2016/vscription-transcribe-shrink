6ab2b7e23ecfaf3b9eb648d24cf95703aa003770
Hossam <hacker2894@gmail.com>
2020-08-14 16:52:46 +0200

    refactor: constants.dev.php update

--- transcribe/data/parts/constants_dev.php
+++ transcribe/data/parts/constants_dev.php
@@ -1,38 +1,13 @@
 <?php
 
-$DEBUG = true; // todo turn off on production
-if(isset($_SESSION['data']['lang']))
-{
-	$lang = $_SESSION['data']['lang'];
-}
-else
-{
-	$_SESSION['data']['lang'] = 'en';
-	$lang = 'en';
-}
-//echo 'lang = '. $lang;
-switch($lang)
-{
-	case 'en':
-		$chtml = 'lang="en"';
-		
-		//OPTIONS
-		$version_control ="2.5";
-		$fileNameSuffix = "-NEEDSREVIEW";
-		$slashShortcut = "Expand Word";
-		$atShortcut = "Dr. List";
-		$clogin = "Login";
-		$cemail = "Email";
-		$cpassword = "Password";
-		$cbaselink = "https://vscriptiontranscribeupload.local"; //for password reset without ending slash
-		
-		$msgVerifyAccount= "Please verify your account first by following the link that was sent to your Email.";
-		
+$DEBUG = false; // todo turn off on production
+global $cbaselink;
+$_SESSION['data']['lang'] = 'en';
 
-		
-		break;
-		
-//	case 'ar':
-		
-//		break;
-}
+$chtml = 'lang="en"';
+
+//OPTIONS
+$version_control = "3";
+$fileNameSuffix = "-NEEDSREVIEW";
+$slashShortcut = "Expand Word";
+$cbaselink = "https://pro.vtex"; //for password reset without ending slash
