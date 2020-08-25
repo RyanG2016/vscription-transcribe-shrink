70639b99bbe77b76facd60fe866b170e5b2b406a
Hossam <hacker2894@gmail.com>
2020-08-25 04:10:03 +0200

    fix <navbar>: lower case email

--- transcribe/data/parts/nav.php
+++ transcribe/data/parts/nav.php
@@ -5,7 +5,7 @@
     </a>
 
     <span class="navbar-text">
-    <?php echo ucfirst($_SESSION['uEmail']);
+    <?php echo strtolower($_SESSION['uEmail']);
     echo isset($_SESSION["acc_name"]) ?  " - " .  $_SESSION["acc_name"] : "";
     echo isset($_SESSION["role_desc"]) ? " - " . $_SESSION["role_desc"] : "";
 
