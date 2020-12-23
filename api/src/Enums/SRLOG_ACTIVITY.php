c810d7caa1d3842912cd46542efaee459bf90865
Hossam <hacker2894@gmail.com>
2020-12-23 20:17:06 +0200

    WIP: more SRLOG_ACTIVITY enum values

--- api/src/Enums/SRLOG_ACTIVITY.php
+++ api/src/Enums/SRLOG_ACTIVITY.php
@@ -10,7 +10,8 @@ class SRLOG_ACTIVITY extends Enum
     const PROCESSING = "rev.ai processing file";
     const COMPLETE = "Complete";
     const FAILED = "Failed";
-    const REVAI_RESPONSE_ERROR = "Rev.ai response error";
+    const REVAI_SEND_ERROR = "rev.ai send error";
+//    const REVAI_RECEIVE_ERROR = "rev.ai file processing failed";
     const MANUAL_REVISION_REQ = "Manual Revision Required";
     const INSUFFICIENT_BALANCE = "Insufficient Balance";
     const COULD_COPY_TO_TEMP = "Couldn't copy to temp dir";
@@ -19,4 +20,6 @@ class SRLOG_ACTIVITY extends Enum
     const REFUND_DIFF_MINUTES = "Refund minutes";
     const DEDUCT_DIFF_MINUTES = "Deduct minutes";
     const ADDED_MINUTES_TO_ACC = self::REFUND_DIFF_MINUTES;
+    const REVAI_ID_NOT_FOUND = "rev.ai returned ID not found in db";
+    const UNKNOWN_WEBHOOK_BODY = "unknown webhook request body";
 }
