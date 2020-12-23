6ad82abfa66dbe82c65f1bae4ff86ada5b2cd5ee
Hossam <hacker2894@gmail.com>
2020-12-23 23:34:35 +0200

    WIP: added SRQ_STATUS.INTERNAL_PROCESSING enum

--- api/src/Enums/SRQ_STATUS.php
+++ api/src/Enums/SRQ_STATUS.php
@@ -6,12 +6,13 @@ use MyCLabs\Enum\Enum;
 
 class SRQ_STATUS extends Enum
 {
-    const QUEUED = 0;
-    const PROCESSING = 1;
-    const COMPLETE = 2;
-    const FAILED = 3;
-    const MANUAL_REVISION_REQ = 5;
-    const INSUFFICIENT_BALANCE = 6;
-    const REVAI_FAILED_TO_RESPOND_WITH_SUCCESS = 7;
-    const WAITING_SWITCH_CONVERT = 8;
+    const QUEUED = 0;            // queue for sending to rev.ai
+    const PROCESSING = 1;        // sent to rev.ai
+    const COMPLETE = 2;          // SR is complete
+    const FAILED = 3;            // SR failed reason should be in notes
+    const MANUAL_REVISION_REQ = 5; // not used till now
+    const INSUFFICIENT_BALANCE = 6; // Account doesn't have sufficient minute balance, file won't be sent to rev.ai and switched to normal transcription
+    const REVAI_FAILED_TO_RESPOND_WITH_SUCCESS = 7; // max retries to send to rev.ai servers reached, file sent to normal trans.
+    const WAITING_SWITCH_CONVERT = 8; // waiting for conversionCron
+    const INTERNAL_PROCESSING = 9; // File is queued for vtt/transcript fetch from rev.ai server | rev.ai OK
 }
