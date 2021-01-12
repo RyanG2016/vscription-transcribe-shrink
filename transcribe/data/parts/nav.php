2bc405c6e4ca41d77c26ee7197010b1e26fb7c9b
Hossam Elwahsh <hacker2894@gmail.com>
2021-01-12 11:06:54 +0200

    refactor <nav>: moved all scripts to nav.js

--- transcribe/data/parts/nav.php
+++ transcribe/data/parts/nav.php
@@ -253,40 +253,6 @@
     </ul><!-- List Group END-->
 </div><!-- sidebar-container END -->
 
-<script type="text/javascript">
-    // Hide submenus
-    $('#body-row .collapse').collapse('hide');
-
-    // Collapse/Expand icon
-    $('#collapse-icon').addClass('fa-angle-double-right');
-
-    // Collapse click
-    $('[data-toggle=sidebar-colapse]').click(function() {
-        SidebarCollapse();
-    });
-
-    function SidebarCollapse () {
-        $('.menu-collapsed').toggleClass('d-none');
-        $('.sidebar-submenu').toggleClass('d-none');
-        $('.submenu-icon').toggleClass('d-none');
-        $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed col-2 col');
-        // $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed col-2 col-auto');
-        $(".vspt-page-container").toggleClass("col-10 vspt-col-auto-fix");
-
-        // Treating d-flex/d-none on separators with title
-        var SeparatorTitle = $('.sidebar-separator-title');
-        if ( SeparatorTitle.hasClass('d-flex') ) {
-            SeparatorTitle.removeClass('d-flex');
-        } else {
-            SeparatorTitle.addClass('d-flex');
-        }
-
-        // Collapse/Expand icon
-        $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
-    }
-</script>
-
-
 <div class="modal" tabindex="-1" id="changeRole">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
--- transcribe/data/scripts/nav.js
+++ transcribe/data/scripts/nav.js
@@ -304,3 +304,35 @@ $("#sidebar-container > ul > a").each(
         });
     }
 );
+
+/* NAV UI Related */
+// Hide submenus
+$('#body-row .collapse').collapse('hide');
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-right');
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed col-2 col');
+    // $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed col-2 col-auto');
+    $(".vspt-page-container").toggleClass("col-10 vspt-col-auto-fix");
+
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
