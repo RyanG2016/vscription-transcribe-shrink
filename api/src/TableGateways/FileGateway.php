e86d082104d47ae3accf9b431db987c35f167968
Hossam Elwahsh <hacker2894@gmail.com>
2021-01-23 03:38:27 +0200

    Revert "DEBUG: prev"
    This reverts commit d131e4dab516b80d00d52453eb25941946b3acec.

--- api/src/TableGateways/FileGateway.php
+++ api/src/TableGateways/FileGateway.php
@@ -428,8 +428,6 @@ class FileGateway implements GatewayInterface
             }
         }
 
-        echo "if you see this you have correctly pulled the debug version of upload";
-
         $jobPrefix = $this->getAccountPrefix($acc_id);
         if(!$jobPrefix)
         {
