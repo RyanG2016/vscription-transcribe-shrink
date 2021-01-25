e7b0ae766018af5ef1cc93f0a3f7c6c5f2472f77
Hossam Elwahsh <hacker2894@gmail.com>
2021-01-25 22:01:04 +0200

    fix: removing zoho rtf download

---
+++ transcribe/data/parts/head_without_zoho.php
@@ -0,0 +1,24 @@
+<?php
+
+include('session_settings.php');
+require('ping.php');
+
+if(!isset($_SESSION['loggedIn']))
+{
+	header('location:logout.php');
+	exit();
+}
+else if((!isset($_SESSION['landed']) || $_SESSION['landed'] == false ) && $vtex_page != \Src\Enums\INTERNAL_PAGES::LANDING)
+{
+    header('location:landing.php');
+    exit();
+}
+if(isset($_SESSION['counter']))
+{
+	unset($_SESSION['counter']);
+}
+
+/*
+else {
+	
+}*/
