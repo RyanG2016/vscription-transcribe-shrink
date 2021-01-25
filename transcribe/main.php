140b87a3212a90a9a88951f6ed0b681fad9c0c4c
Hossam Elwahsh <hacker2894@gmail.com>
2021-01-26 00:02:41 +0200

    Update main.php remove need help link

--- transcribe/main.php
+++ transcribe/main.php
@@ -137,7 +137,7 @@ else {
 
                 <div class="vtex-top-bar form-row m-0">
                     <h2 class="users-tbl-title col">Jobs List</h2>
-                    <a href="https://vscriptionpro.helpdocsonline.com/" class="col-auto mt-auto mb-auto" target="_blank" title="">Need help <i class="far fa-question-circle"></i></a>
+<!--                    <a href="https://vscriptionpro.helpdocsonline.com/" class="col-auto mt-auto mb-auto" target="_blank" title="">Need help <i class="far fa-question-circle"></i></a>-->
 
                     <button class="mdc-button mdc-button--unelevated mr-2 foo-button" id="newupload_btn">
                         <div class="mdc-button__ripple"></div>
