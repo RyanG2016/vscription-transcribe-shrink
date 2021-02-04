957901ffb75716899cccae0362d4b91657b9f5d2
Hossam Elwahsh <hacker2894@gmail.com>
2021-02-04 18:44:17 +0200

    update <nav>: changed collapse expand icon

--- transcribe/data/scripts/nav.js
+++ transcribe/data/scripts/nav.js
@@ -328,7 +328,7 @@ $(document).ready(function(){
     $('#body-row .collapse').collapse('hide');
 
 // Collapse/Expand icon
-    navCollapseIcon.addClass('fa-angle-double-right');
+    navCollapseIcon.addClass('fa-chevron-double-right');
 
 
     function SidebarCollapse (silent = false) {
@@ -349,7 +349,7 @@ $(document).ready(function(){
         }
 
         // Collapse/Expand icon
-        navCollapseIcon.toggleClass('fa-angle-double-left fa-angle-double-right');
+        navCollapseIcon.toggleClass('fa-chevron-double-right fa-chevron-double-left');
         navCollapseText.html(navCollapseText.html() === "Expand"?"Collapse":"Expand");
 
         navCollapseIcon.parent().parent().popover('dispose').popover({
