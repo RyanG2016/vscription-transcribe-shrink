d182bab464748e621e9db76179b4797dc3242eb0
Hossam Elwahsh <hacker2894@gmail.com>
2021-03-04 01:11:12 +0200

    gitignore: add newly added js files sources

--- transcribe/data/scripts/packages.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    $(".package-card-bottom").on("click", function() {
-        $("#package").attr('value', $(this).attr("id"));
-        $("#purchase").submit();
-    });
-
-});
