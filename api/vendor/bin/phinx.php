d0c0ef554fa6dc80fcb4e67a44a6118bb67452c2
Hossam Elwahsh <hacker2894@gmail.com>
2021-03-07 19:38:15 +0200

    init: phinx test migration and config files

---
+++ api/vendor/bin/phinx.php
@@ -0,0 +1,42 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/../../phinx/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/../../phinx/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'production_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'vtexvsi_transcribe',
+            'user' => 'admin',
+            'pass' => 'EJT2Dz59XFU6DFJX',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'test' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'testing_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
