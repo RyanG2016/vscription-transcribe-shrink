f25900bc3368457b3f44777440f856301e82b771
Ryan Gaudet <rgaudet@vtexvsi.com>
2021-03-30 13:34:22 -0500

    chore: Removing old DB maintenance scripts

--- jobs/check_exceeded_retention/typetest.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-REM Directory Checking test
-
-set audiofolder=C:\apache24\htdocs\vscription-transcribe-temp\uploads\
-set audiofile=C:\apache24\htdocs\vscription-transcribe-temp\uploads\F11887_VT4096_Recognition_Test_1.m4a
-cd
-
-cd %audiofolder%
-cd
