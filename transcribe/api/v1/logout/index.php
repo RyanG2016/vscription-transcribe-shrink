5d02eb9d800b275288c74f5a752d5ae6549b597a
Hossam Elwahsh <hacker2894@gmail.com>
2021-06-03 05:24:58 +0200

    update <logout>: return type json for not logged in msg

--- transcribe/api/v1/logout/index.php
+++ transcribe/api/v1/logout/index.php
@@ -6,6 +6,12 @@ include('../../../data/parts/session_settings.php');
 
 require('../../../data/parts/ping.php');
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 if(!isset($_SESSION['loggedIn']))
 {
     header("HTTP/1.1 200 OK");
@@ -18,11 +24,6 @@ if(!isset($_SESSION['loggedIn']))
 
 use Src\Controller\LogoutController;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
