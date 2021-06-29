b6218820f21e60ed4c901401b9172e64556cf691
Hossam Elwahsh <hacker2894@gmail.com>
2021-06-29 06:15:37 +0200

    #32 duplicate scrollbars in manage users

--- transcribe/data/scripts/manage_users.js
+++ transcribe/data/scripts/manage_users.js
@@ -64,18 +64,6 @@ $(document).ready(function () {
 
     });
 
-    $("body").niceScroll({
-        hwacceleration: true,
-        smoothscroll: true,
-        cursorcolor: "white",
-        cursorborder: 0,
-        scrollspeed: 10,
-        mousescrollstep: 20,
-        cursoropacitymax: 0.7
-        //  cursorwidth: 16
-
-    });
-
     $.ajaxSetup({
         cache: false
     });
