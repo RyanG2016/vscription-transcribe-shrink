24b38b49958bc12d7e1be239f004cf591e44f178
RyanG2016 <rgaudet@vtexvsi.com>
2021-09-16 11:11:16 -0500

    fix. Updated billing_reports.php and typist_reports.php to use min.js files as .js files don't exist on main

--- transcribe/panel/billing_report.php
+++ transcribe/panel/billing_report.php
@@ -68,7 +68,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != "1") {
 
 
 <!--    <script src="../data/scripts/billing_report.min.js?v=--><!--"></script>-->
-    <script src="../data/scripts/billing_report.js"></script>
+    <script src="../data/scripts/billing_report.min.js"></script>
     <script src="../data/thirdparty/scripts/html2pdf.bundle.min.js"></script>
     <link href="https://printjs-4de6.kxcdn.com/print.min.css" rel="stylesheet">
     <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
